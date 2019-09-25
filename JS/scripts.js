28e89fdb0b0498ceee1d2d9ff0442f672ebfb384
Sarado Touch <silumtao2005@gmail.com>
2019-09-24 19:16:39 -0700

    adding lang alert code

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,18 +1,24 @@
-$(document).ready(function) {
-  $("#form-control").submit(function)(event) {
-    var javascriptInput = $("input#javascript").val()
+jQuery(document).ready(function() {
+  jQuery("#languages").submit(function() {
+    alert ("What language would you like to learn?")
+
+    if () {
+      else if () {
+        else {
+
+        }
+      }
+    }
+    var languagestInput = $("input#languages").val()
+
     var pythonInput = $("input#python").val()
     var cplusInput = $("input#cplus").val()
     var csharpeInput = $("input#csharp").val()
     var rubyInput = $("input#ruby").val()
 
-    $(".javascript").text(javascriptinput);
-    $(".python").text(pythoninput);
-    $(".cplus").text(cplusinput);
-    $(".csharpe").text(csharpinput);
-    $(".ruby").text(rubyinput);
 
-    $("#form-control").show();
+    $("#languages").show();
 
     event.preventDefault();
-  }
+});
+});
