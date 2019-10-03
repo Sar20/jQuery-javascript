4595ca941055c00045b49c360a69dca4296cf50b
Sarado Touch <silumtao2005@gmail.com>
2019-10-03 13:44:21 -0700

    updated folder

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,23 +1,16 @@
 jQuery(document).ready(function() {
-  jQuery("#languages").submit(function() {
+jQuery("form").submit(function(event) {
     alert ("What language would you like to learn?")
 
-    if () {
-      else if () {
-        else {
-
-        }
-      }
-    }
-    var languagestInput = $("input#languages").val()
-
-    var pythonInput = $("input#python").val()
-    var cplusInput = $("input#cplus").val()
-    var csharpeInput = $("input#csharp").val()
-    var rubyInput = $("input#ruby").val()
-
-
-    $("#languages").show();
+      if id = "language"
+    // use condition heref () {
+    //   else if () {
+    //     else {
+    //
+    //     }
+    //   }
+    // }
+    jQuery("#languages").show();
 
     event.preventDefault();
 });
